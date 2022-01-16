b980d9e7c468a1c67f93496f07db21b7f766bedf
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 03:36:07 +0100

    bad fit

--- 1.py
+++ 1.py
@@ -44,18 +44,21 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 
-a = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
-b = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+# a = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
+# b = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+
+a = [89, 43, 36, 36, 95, 10, 66, 34, 38, 20, 26, 29, 48, 64, 6, 5, 36, 66, 72, 40]
+b = [21, 46, 3, 35, 67, 95, 53, 72, 58, 10, 26, 34, 90, 33, 38, 20, 56, 2, 47, 15]
 
 slope, intercept, r, p, std_err = stats.linregress(a, b)
 def c(d):
     return slope * d + intercept
 
-# e = list(map(c, a))
+e = list(map(c, a))
 
-# plt.scatter(a, b)
-# plt.plot(a, e)
-# plt.show()
+plt.scatter(a, b)
+plt.plot(a, e)
+plt.show()
 
-e = c(10)
-print(e)
+# e = c(10)
+# print(e)
