ac4bd681ad724a10b9c2d979931a16c8fdbdea32
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 02:52:43 +0100

    percentile

--- 1.py
+++ 1.py
@@ -11,12 +11,24 @@
 # d = stats.mode(a)
 # print(d)
 
+# import numpy
+
+# a = [86, 87, 88, 86, 87, 85, 86]
+# b = numpy.std(a)
+# print(b)
+
+# c = [32, 111, 138, 28, 59, 77, 97]
+# d = numpy.std(c)
+# print(d)
+
+# e = numpy.var(c)
+# print(e)
+
 import numpy
 
-a = [86, 87, 88, 86, 87, 85, 86]
-b = numpy.std(a)
+a = [5, 31, 43, 48, 50, 41, 7, 11, 15, 39, 80, 82, 32, 2, 8, 6, 25, 36, 27, 61, 31]
+b = numpy.percentile(a, 75)
 print(b)
 
-c = [32, 111, 138, 28, 59, 77, 97]
-d = numpy.std(c)
-print(d)
+c = numpy.percentile(a, 90)
+print(c)
