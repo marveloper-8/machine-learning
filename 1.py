dd13d80d785e8973cbac872fa6c09c3f23f57a37
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 01:24:36 +0100

    find mode (the number that appears the most) on the array

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
 import numpy
+from scipy import stats
 
-a = [99, 86, 87, 88, 86, 103, 87, 94, 78, 77, 85, 86]
+a = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
 b = numpy.mean(a)
 print(b)
 
 c = numpy.median(a)
 print(c)
+
+d = stats.mode(a)
+print(d)
