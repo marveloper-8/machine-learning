127ddc22d844fb2b9a9d29b5c9001e0bba7fa64d
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 02:39:44 +0100

    standard deviation

--- 1.py
+++ 1.py
@@ -1,12 +1,22 @@
-import numpy
-from scipy import stats
+# import numpy
+# from scipy import stats
 
-a = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
-b = numpy.mean(a)
+# a = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+# b = numpy.mean(a)
+# print(b)
+
+# c = numpy.median(a)
+# print(c)
+
+# d = stats.mode(a)
+# print(d)
+
+import numpy
+
+a = [86, 87, 88, 86, 87, 85, 86]
+b = numpy.std(a)
 print(b)
 
-c = numpy.median(a)
-print(c)
-
-d = stats.mode(a)
+c = [32, 111, 138, 28, 59, 77, 97]
+d = numpy.std(c)
 print(d)
