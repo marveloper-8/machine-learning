3d2826530edfc93a48c78b874b8db7a0e63ca87f
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 03:16:29 +0100

    relationship

--- 1.py
+++ 1.py
@@ -48,11 +48,13 @@ a = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
 b = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
 
 slope, intercept, r, p, std_err = stats.linregress(a, b)
-def c(d):
-    return slope * d + intercept
+# def c(d):
+#     return slope * d + intercept
 
-e = list(map(c, a))
+# e = list(map(c, a))
 
-plt.scatter(a, b)
-plt.plot(a, e)
-plt.show()
+# plt.scatter(a, b)
+# plt.plot(a, e)
+# plt.show()
+
+print(r)
